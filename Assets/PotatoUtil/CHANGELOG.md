5660f8177f7281c3484e51a6320ff1f54f9593fe
Terra Lauterbach <terra.lauterbach@gmail.com>
2020-10-14 17:45:53 -0500

    added IndexedSet<T> data structure

--- Assets/PotatoUtil/CHANGELOG.md
+++ Assets/PotatoUtil/CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.0] - 2020-10-14
+### Added
+- New data structure `IndexedSet<T>` that implements `System.Collections.IReadOnly<T>` that doesn't have the option to cast to an `IList<T>` like `System.Collections.ObjectModel.ReadOnlyList<T>`. The new data structure was added to disallow the option to do the casting and explicity maintain the readonly state of the set by not even given the option to cast to `IList<T>`.
+
 ## [1.0.0] - 2020-09-08
 ### Added
 - Dependencies to `com.potatointeractive.pcg` and `com.beauprime.beaudata` packages
---
+++ Assets/PotatoUtil/Scripts/DataStructures/IndexedSet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PotatoUtil {
+
+	/// <summary>
+	/// A simple, wrapped implementation of the IReadOnlyList<typeparamref name="T"/> 
+	/// interface that has more practical construction options
+	/// </summary>
+	public class IndexedSet<T> : IReadOnlyList<T> {
+		
+		public T this[int index] {
+			get {
+				if (index < 0 || index >= Count) {
+					throw new IndexOutOfRangeException();
+				}
+				return m_set[index];
+			}
+		}
+
+		public int Count { get { return m_set.Length; } }
+
+		private T[] m_set;
+
+		public IndexedSet(IList<T> collection) : this((IEnumerable<T>)collection) {
+		}
+		public IndexedSet(params T[] collection) : this((IEnumerable<T>)collection) {
+		}
+		public IndexedSet(IEnumerable<T> collection) {
+			int count = 0;
+			foreach (T t in collection) {
+				count++;
+			}
+			m_set = new T[count];
+			foreach (T t in collection) {
+				m_set[m_set.Length - (count--)] = t;
+			}
+		}
+
+		public int IndexOf(T item) {
+			return Array.IndexOf(m_set, item);
+		}
+
+		public IEnumerator<T> GetEnumerator() {
+			return ((IEnumerable<T>)m_set).GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return m_set.GetEnumerator();
+		}
+	}
+
+}
