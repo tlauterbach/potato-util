7c4cd8df6cdd21b4735f18ca5e063353fce88734
Terra Lauterbach <terra.lauterbach@gmail.com>
2021-01-14 23:17:23 -0600

    added NonDrawingGraphic behaviour

--- Assets/PotatoUtil/CHANGELOG.md
+++ Assets/PotatoUtil/CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.5.0] - 2021-01-14
+### Added
+- `NonDrawingGraphic` and `NonDrawingGraphicEditor` courtesy of Slipp Douglas Thompson's [Gist](https://gist.github.com/capnslipp/349c18283f2fea316369).
+- `PotatoUtil.Editor` namespace for Editor only scripts and utilities.
+
 ## [1.4.1] - 2021-01-02
 ### Fixed
 - `Base32` defect where `Parse` would always fail with correct length strings
---
+++ Assets/PotatoUtil/Scripts/Editor/NonDrawingGraphicEditor.cs
@@ -0,0 +1,26 @@
+﻿/// @creator: Slipp Douglas Thompson
+/// @license: Public Domain per The Unlicense.  See <http://unlicense.org/>.
+/// @purpose: Slimmed-down Inspector UI for `NonDrawingGraphic` class.
+/// @why: Because this functionality should be built-into Unity.
+/// @usage: Add a `NonDrawingGraphic` component to the GameObject you want clickable, but without its own image/graphics.
+/// @interwebsouce: https://gist.github.com/capnslipp/349c18283f2fea316369
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+using PotatoUtil;
+
+namespace PotatoUtil.Editor {
+	
+	[CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+	public class NonDrawingGraphicEditor : GraphicEditor {
+		public override void OnInspectorGUI() {
+			base.serializedObject.Update();
+			EditorGUILayout.PropertyField(base.m_Script, new GUILayoutOption[0]);
+			// skipping AppearanceControlsGUI
+			base.RaycastControlsGUI();
+			base.serializedObject.ApplyModifiedProperties();
+		}
+	}
+
+}
